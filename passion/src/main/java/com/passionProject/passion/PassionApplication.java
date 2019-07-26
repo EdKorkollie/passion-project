eb0dfd4050294bf7851eb5854be50b434a21d11f
Edward Korkollie <korkollieed@gmail.com>
2019-07-26 18:34:43 -0400

    first commit

---
+++ passion/src/main/java/com/passionProject/passion/PassionApplication.java
@@ -0,0 +1,13 @@
+package com.passionProject.passion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassionApplication.class, args);
+	}
+
+}
